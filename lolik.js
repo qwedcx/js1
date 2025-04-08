efa309d9221a775dc86868c98d4d914a3cbf8450
qwedcx <kiov7635@gmail.com>
2025-04-08 11:32:45 +0500

    simple programs

---
+++ lolik.js
@@ -0,0 +1,11 @@
+//alert("Сумма равна " + (sum = parseFloat(prompt("Введите первое число: ", 0)) + parseFloat(prompt("Введите второе число: ", 0))));
+//alert(numOfPages = Math.ceil(parseInt(prompt("Введите количество фото в папке: ", 0)) / parseInt(prompt("Введите количество на 1 странице: ", 0))))
+// let number, d1,d2,d3,d4,d5,d6;
+// number = parseInt(prompt("Введите 6 значное число", 111111))
+// d1 = number % 10;
+// d2 = Math.floor(number / 10) % 10
+// d3 = Math.floor(number / 100) % 10
+// d4 = Math.floor(number / 1000) % 10
+// d5 = Math.floor(number / 10000) % 10
+// d6 = Math.floor(number / 100000) % 10
+// alert(d1+d2+d3+d4+d5+d6)
